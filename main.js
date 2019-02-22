536c97e44c709be3d6da31086d9e1676daeeb84b
Matej69 <mlelek94@gmail.com>
2019-02-22 21:37:04 +0100

    switched to phantomjs and created click event that works but bullshit memory leak is a problem

--- main.js
+++ main.js
@@ -1,125 +1,33 @@
 var webPage = require('webpage');
 var page = webPage.create();
+page.settings.loadImages = false;
 
 
 
-/*
+
 page.open('https://www.konzum.hr/klik/#!/offers', function(status) {
   console.log('Status: ' + status);
   // Do other things here...
 });
-*/
-
-/**
- * Bad way but shit works
- */
-/*
-var urlWithInfoRerturned = false;
-var urlWithInfo = "https://www.konzum.hr/klik/v2/offers?per_page=48&page=&filter%5Bsort%5D=soldStatisticsDesc";
-var productsContent;
-var parsingDone = false;
-page.onResourceReceived = function(response) {
-  urlWithInfoRerturned = (response.url.indexOf(urlWithInfo) !== -1) ? true : urlWithInfoRerturned;
-  if (urlWithInfoRerturned) {
-    var isThisUrlWithInfo = (response.url == urlWithInfo);
-    if (urlWithInfoRerturned && !isThisUrlWithInfo && !parsingDone) {
-      productsContent = JSON.stringify(page.plainText);
-      console.log(productsContent);
-      parsingDone = true;
-    }
-      //productsContent = JSON.stringify(page.plainText);
-    // console.log("\n\n ***************************************" + response.url + "**************************************\n" + JSON.stringify(page.plainText));
-  }
-};
-*/
-
-
-
-/**
- * Better way
- */
-/*
-var urlWithInfo = "https://www.konzum.hr/klik/v2/offers?per_page=48&page=&filter%5Bsort%5D=soldStatisticsDesc";
-var productsContent;
-page.onResourceReceived = function(response) {
-  if (response.url.indexOf("https://www.konzum.hr/klik/v2/offers?per_page=48&page=&filter%5Bsort%5D=soldStatisticsDesc") !== -1) {
-    page.render('google_home.jpeg', {format: 'jpeg', quality: '100'});
-
-      productsContent = JSON.stringify(page.plainText);
-      console.log(productsContent);
-  }
-};
-*/
-
-
-
-/*
-var settings = {
-  method: "GET"
-};
-var url = "https://www.konzum.hr/klik/v2/offers?per_page=48&page=&filter%5Bsort%5D=soldStatisticsDesc";
-page.open(url, settings, function(status) {
-  console.log('Status: ' + status);
-});
 
 page.onResourceReceived = function(response) {
-  console.log(JSON.stringify(page));
+   // console.log('Response (#' + response.url);
 };
-*/
 
 
-/**
- * ********************************** MAIN PROGRAM *********************************
- */
-var casper = require('casper').create();
-var url = 'https://www.konzum.hr/klik/v2/offers?per_page=48&page=&filter%5Bsort%5D=soldStatisticsDesc';
+setTimeout(function() {
+  var html = JSON.stringify(page.content);
+  var s = page.evaluate(function() {
+    //return document.querySelector('.news-icon');
 
-casper.start();
+    return document.querySelector('.pagination');
+    /*
+    var e = document.createEvent('MouseEvents');
+    e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e);
+    */
 
-casper.thenOpen(url, function() {
-    this.open(url, {
-        method: 'get',
-        headers: {
-            'Accept': 'application/json'
-        }
-    });
-});
-
-casper.then(function () {
-  var filteredRes = getFilteredProductDataObj(this.getPageContent());
-  console.log(JSON.stringify(filteredRes));
-  this.exit();
-});
-
-// must be the last one so it can scan for all thne/promise calls and initialise casper tasks stack
-casper.run();
-
-
-
-
-
-
-/**
- * ********************************** UTIL FUNCTION *********************************
- */
-function getFilteredProductDataObj(responseStr) {
-  var obj = JSON.parse(responseStr);
-  var fitleredObj = obj.products.map(function(item) {
-    return {
-      name: item.name,
-      price: item.price.formatted_price
-    };
   });
-  return fitleredObj;
-}
+  console.log(JSON.stringify(s.innerHTML));
+}, 10000)
 
-
-
-
-
-
-
-
-  
-
-  
