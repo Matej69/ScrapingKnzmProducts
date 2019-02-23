44a1dbfee4d26a3c2fead5240dfa26869acc3dcc
Matej69 <mlelek94@gmail.com>
2019-02-23 01:30:52 +0100

    waitUntil: networkidle0 added so we wait until other packages arrive

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ const puppeteer = require('puppeteer');
 async function run() {
   console.log("started");
   const browser = await puppeteer.launch({
-    headless: false
+    headless: true
   });
   console.log("lunched");
   const page = await browser.newPage();
   console.log("paged");
 
-  await page.goto('https://www.konzum.hr/klik/#!/offers');
+  await page.goto('https://www.konzum.hr/klik/#!/offers', {"waitUntil" : "networkidle0"});
   console.log("goto");
 
-  const val = await page.$eval('.ukupno', (element) => {
-    return element.innerText
+  const val = await page.$eval('.ime-proizvoda', (element) => {
+    return element.innerHTML
   })
   console.log(val);
   
