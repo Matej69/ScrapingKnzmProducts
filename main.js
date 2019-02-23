67522d6d528dd21a5622d98fabf5e15a2fb8c867
Matej69 <mlelek94@gmail.com>
2019-02-23 03:31:20 +0100

    extraction of konzum object sort of works but still sucks

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const puppeteer = require('puppeteer');
 
+
+
+
+/**
+ * ***************************** UTILS FUNCTIONS ******************************
+ */
+
+
+
+
 async function run() {
   console.log("started");
   const browser = await puppeteer.launch({
@@ -13,13 +23,26 @@ async function run() {
   console.log("goto");
 
   const productNameList = await page.evaluate( () => 
-    Array.from(
-      document.querySelectorAll('.ime-proizvoda'), element => element.innerText 
-    )
+    Array.from(document.querySelectorAll('.inner-proizvod'), element => {
+      var lipe = element.querySelector(".decimalni-dio");
+      var kn = element.querySelector(".cijena");
+      kn.removeChild(lipe);
+      kn.removeChild(element.querySelector(".tekstualni-dio"));
+      return { 
+        name: element.querySelector(".ime-proizvoda").innerText,
+        //price: ChildrenToRemove(element.querySelector(".cijena"),[".cijena span"]).innerText
+        //pricelp: element.querySelector(".cijena .decimalni-dio").innerText,
+        priceBase: kn.innerText,
+        priceExtension: lipe.innerText
+      };      
+    })
   );
+  /*
   productNameList.forEach(prod => {
     console.log(prod);
   });
+  */
+ console.log(productNameList[0]);
   
   //browser.close();
   console.log("closed");
