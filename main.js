df0148adcdf1ddccdaac11e81ff7657583503eae
Matej69 <mlelek94@gmail.com>
2019-02-23 01:16:24 +0100

    since memory limit sucks I switched to puppeteer + working getting value of paragraph

--- main.js
+++ main.js
@@ -1,33 +1,24 @@
-var webPage = require('webpage');
-var page = webPage.create();
-page.settings.loadImages = false;
+const puppeteer = require('puppeteer');
 
-
-
-
-page.open('https://www.konzum.hr/klik/#!/offers', function(status) {
-  console.log('Status: ' + status);
-  // Do other things here...
-});
-
-page.onResourceReceived = function(response) {
-   // console.log('Response (#' + response.url);
-};
-
-
-setTimeout(function() {
-  var html = JSON.stringify(page.content);
-  var s = page.evaluate(function() {
-    //return document.querySelector('.news-icon');
-
-    return document.querySelector('.pagination');
-    /*
-    var e = document.createEvent('MouseEvents');
-    e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    a.dispatchEvent(e);
-    */
-
+async function run() {
+  console.log("started");
+  const browser = await puppeteer.launch({
+    headless: false
   });
-  console.log(JSON.stringify(s.innerHTML));
-}, 10000)
+  console.log("lunched");
+  const page = await browser.newPage();
+  console.log("paged");
 
+  await page.goto('https://www.konzum.hr/klik/#!/offers');
+  console.log("goto");
+
+  const val = await page.$eval('.ukupno', (element) => {
+    return element.innerText
+  })
+  console.log(val);
+  
+  //browser.close();
+  console.log("closed");
+}
+
+run();
