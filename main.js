8eeb58870a14623b3b9e51ea5b2651f3fc1e2b1e
Matej69 <mlelek94@gmail.com>
2019-02-24 15:41:49 +0100

    images of product saved as .jpg and their name added to json as imgID attrivute value

--- main.js
+++ main.js
@@ -8,6 +8,17 @@ function WaitFor(x) {
   });
 }
 
+const assert = require('assert');
+const fs = require('fs');
+const getImageContent = async (page, url) => {
+  const { content, base64Encoded } = await page._client.send(
+    'Page.getResourceContent',
+    { frameId: String(page.mainFrame()._id), url },
+  );
+  assert.equal(base64Encoded, true);
+  return content;
+};
+
 async function run() {
   const browser = await puppeteer.launch({
     headless: true
@@ -24,23 +35,10 @@ async function run() {
   console.log("Request sent for https://www.konzum.hr/klik/#!/offers");
   await page.goto('https://www.konzum.hr/klik/#!/offers', {"waitUntil" : "networkidle0"});
 
-
-  try {
-    const tree = await page._client.send('Page.getResourceTree');
-    for (const resource of tree.frameTree.resources) {
-      console.log(resource);
-    }
-  } catch (e) {
-    console.log(e);
-  }
-
-  
-
-
-/*
+  var imgID = 0;
   var isOnLastPage;
   do {
-       // get all products on a page
+       // get all products data on a page
       const productNameList = await page.evaluate( () => 
       Array.from(document.querySelectorAll('.inner-proizvod'), element => {
         var productName = element.querySelector(".ime-proizvoda");
@@ -49,17 +47,35 @@ async function run() {
         var priceBase = element.querySelector(".cijena");
             priceBase.removeChild(priceExtension);
             priceBase.removeChild(priceText);
+        var imgSrc = element.querySelector(".image img").src;
         return { 
           name: productName.innerText,
           priceBase: priceBase.innerText,
           priceExtension: priceExtension.innerText,
-          priceText: priceText.innerText
+          priceText: priceText.innerText,
+          imgSrc: imgSrc,
+          imgID: ''
         };
       })
     );
-    productNameList.forEach(prod => {
-      console.log(productNameList);
-    });
+   //Save img to file and generate imgID for json
+   try {
+    console.log("Saving " + productNameList.length + " images to file");
+    for (let i = 0; i < productNameList.length; i++) {
+      productNameList[i].imgID = imgID++;
+      const content = await getImageContent(page, productNameList[i].imgSrc);
+      const contentBuffer = Buffer.from(content, 'base64');
+      fs.writeFileSync(imgID + '.jpg', contentBuffer, 'base64');
+    }
+   }
+   catch (e) {
+    console.log(e);
+  }
+  console.log("Images saved");
+  // print JSON product data
+  productNameList.forEach(prod => {
+    console.log(prod);
+  });
 
     // go to next page
     isOnLastPage = await page.evaluate( () => {
@@ -76,7 +92,7 @@ async function run() {
     await WaitFor(5000);
   }
   while (!isOnLastPage);
-  */
+  
   
   //browser.close();
   console.log("closed");
