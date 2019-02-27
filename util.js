15e3debbd3a50ec0959fb738d76a31dd86e605f4
Matej69 <mlelek94@gmail.com>
2019-02-27 22:51:41 +0100

    switched from extracting base64 image to just saving image url to firebase

--- util.js
+++ util.js
@@ -1,8 +1,5 @@
-module.exports = { 
-  WaitFor, 
-  GetImageAsBase64, 
-  GoToNextPage, 
-  GetImageDataAsBase64, 
+module.exports = {
+  GoToNextPage,
   ExtractProductDataFromPage,
   PrintScrapedData  
 };
@@ -18,16 +15,6 @@ function WaitFor(x) {
 }
 
 
-async function GetImageAsBase64(page, url) {
-  const assert = require('assert');
-
-  const { content, base64Encoded } = await page._client.send(
-    'Page.getResourceContent',
-    { frameId: String(page.mainFrame()._id), url },
-  );
-  assert.equal(base64Encoded, true);
-  return content;
-}
 
 /**
  * Tries to go to the next page and returns isOnLastPage info about current page(before switching).
@@ -49,23 +36,10 @@ async function GoToNextPage(page) {
 }
 
 /**
- * Save img to file and generate imgID for json
- */
-async function GetImageDataAsBase64(page, imgSrc) {
-  try {
-    const imageBase64 = await GetImageAsBase64(page, imgSrc);
-    return imageBase64;
-  }
-  catch (e) {
-    console.log(e);
-  }
-}
-
-/**
  * Get all products data from a current page
  */
 async function ExtractProductDataFromPage(page) {
-  var productsData = await page.evaluate( () => 
+  return await page.evaluate( () => 
     Array.from(document.querySelectorAll('.inner-proizvod'), element => {
       var productName = element.querySelector(".ime-proizvoda");
       var priceExtension = element.querySelector(".decimalni-dio");
@@ -79,15 +53,10 @@ async function ExtractProductDataFromPage(page) {
         priceBase: priceBase.innerText,
         priceExtension: priceExtension.innerText,
         priceText: priceText.innerText,
-        //imgSrc: imgSrc,
-        imgBase64: ''
+        imgSrc: imgSrc
       };
     })
   );
-
-  for(var i = 0; i < productsData.length; ++i)
-    productsData[i].imgBase64 = await GetImageDataAsBase64(page, productsData[i].imgSrc);
-  return productsData;
 }
 
 
